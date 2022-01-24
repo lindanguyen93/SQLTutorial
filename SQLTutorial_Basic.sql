97b9b4cce8857aeb6a65a3283ba3fcd5192b053b
Linda <92672331+lindanguyen93@users.noreply.github.com>
2022-01-25 10:25:58 +1100

    Add files via upload

---
+++ SQLTutorial_Basic.sql
@@ -0,0 +1,87 @@
+--Table 1 Query
+
+Create Table EmployeeDemographics
+(EmployeeID int ,
+FirstName varchar (50),
+LastName varchar (50),
+Age int,
+Gender varchar (50),
+Nationality varchar (50)
+)
+
+
+--Table 2 Query
+Create Table EmployeeSalary
+(EmployeeID int,
+JobTitle varchar (50),
+Salary int )
+
+--Table 1 Insert: 
+Insert into EmployeeDemographics values
+(101, 'Anna','Smith', 27, 'Female', 'Australian'),
+(102, 'Angie', 'Martin', 29, 'Female', 'Australian'),
+(103, 'Phuong', 'Nguyen',41, 'Female', 'Vietnamese'),
+(104, 'John', 'Lee',31, 'Male', 'Australian'),
+(105, 'Burh', 'Saskan',35, 'Male', 'Indian'),
+(106, 'Austin', 'Cooper',36, 'Male', 'American'),
+(107, 'Lily', 'June',28 ,'Female', 'Australian')
+
+
+--Table 2 Insert: 
+
+Insert into EmployeeSalary values
+(101, 'Salesman', 41000),
+(102, 'Receptionist', 30000),
+(103, 'Salesman', 53000),
+(104, 'Accountant', 37000),
+(105, 'HR', 52000),
+(106, 'Regional Manager', 70000),
+(107, 'Supplier Relations', 42000)
+
+
+
+--SELECT STATEMENT: Top, Distinct, Count, As, Max, Min, Avg
+
+Select *
+--Select LastName,LastName
+--Select Top 5 *
+--Select distinct (EmplyeeID)
+--Select distinct (Gender)
+From EmployeeDemographics
+
+
+Select Count (LastName) AS LastNameCount
+--Select Max(Salary) as MaxSalary
+--Select AVG (Salary) as AvgSalary
+From EmployeeSalary
+
+Select *
+From SQLTutorial.dbo.EmployeeSalary
+
+--WHERE STATEMENT: =, <> does not equal, <, >, and, or, like, null, not null, in = equal in multiple things
+
+Select *
+From EmployeeDemographics
+Where FirstName ='Anna' 
+--where FirstName IN ('Anna', 'John')  
+--Where FirstName <> 'Anna' AND LastName IS NOT NULL
+--Where Age >30 and Nationality like '%Aus%' --contain Aus
+--Where FirstName like '%n' --end by letter n,   if 'A%' begin with A
+
+
+--GROUP BY, ORDER BY
+
+Select Gender, Age, COUNT (Gender) As CountGender
+From EmployeeDemographics
+Where Age >30
+Group By Gender, Age
+
+Select *
+From EmployeeDemographics
+Where Age >25
+Order By 4 desc, 5 Desc
+
+
+
+
+
